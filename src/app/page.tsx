cbf7720e2281591c1f944e39a6716d42422cd170
HafizNabeel12 <138764369+HafizNabeel12@users.noreply.github.com>
2025-09-08 06:05:58 +0500

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import Navbar from '@/components/Navbar';
-import Hero from '@/components/Hero';
-import ProductShowcase from '@/components/ProductShowcase';
-import Features from '@/components/Features';
-import Testimonials from '@/components/Testimonials';
-import CallToAction from '@/components/CallToAction';
-import Footer from '@/components/Footer';
+
 import LandingPage from '@/components/LandingPage';
 
 export default function HomePage() {
@@ -14,11 +8,7 @@ export default function HomePage() {
 
       <LandingPage/>
       
-      {/* <Hero /> */}
-      {/* <ProductShowcase /> */}
-      {/* <Features /> */}
-      {/* <Testimonials /> */}
-      {/* <CallToAction /> */}
+     
       
     </main>
   );
